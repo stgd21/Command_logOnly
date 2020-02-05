f7f3938aa982b0c0a22f199abde076731eeb4d6a
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-02-05 12:20:40 -0700

    Add files via upload

---
+++ Scripts/Invoker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invoker
+{
+    private Command m_Command;
+    private Command noCommand = new NoMovement();
+    private float time;
+
+    public void SetCommand(Command command, float timeStart)
+    {
+        m_Command = command;
+        time = timeStart;
+    }
+
+    public void ExecuteCommand()
+    {
+        m_Command.Execute();
+    }
+
+    public void Clear(string key)
+    {
+        m_Command = noCommand;
+        Debug.Log(key + " held for " + (Time.timeSinceLevelLoad - time) + " seconds");
+    }
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Physics Component of player cube
+    public Rigidbody rb;
+    //Forces to set in inspector
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    private bool isRight = false;
+    private bool isLeft = false;
+
+    private Invoker invoker;
+    Command noMovement;
+
+    void Start()
+    {
+        invoker = new Invoker();
+        noMovement = new NoMovement();
+        invoker.SetCommand(noMovement, Time.timeSinceLevelLoad);
+    }
+
+    // FixedUpdate is called evenly across framerates
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("d"))
+        {
+            Command moveRight = new MoveRight(rb, sidewaysForce);
+            invoker.SetCommand(moveRight, Time.timeSinceLevelLoad);
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            Command moveLeft = new MoveLeft(rb, sidewaysForce);
+            invoker.SetCommand(moveLeft, Time.timeSinceLevelLoad);
+        }
+
+        if (Input.GetKeyUp("d"))
+        {
+            invoker.Clear("D");
+            //Debug.Log("D up");
+        }
+
+        if (Input.GetKeyUp("a"))
+        {
+            invoker.Clear("A");
+            //Debug.Log("A up");
+        }
+
+
+        if (rb.position.y < -1f)
+        {
+            //End the game if the player goes off the edge
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        invoker.ExecuteCommand();
+    }
+}
